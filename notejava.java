9813027b8042b5ad4f0f73c292e764d70f77ffa4
Ritesh kaushik <122422966+riteshkaushik1@users.noreply.github.com>
2025-07-02 12:51:49 +0530

    Add files via upload
    File uploaded successfully

---
+++ notejava.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class notejava
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Ritesh Kaushik");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		long Rollno = sc.nextLong();
+	}
+
+}
